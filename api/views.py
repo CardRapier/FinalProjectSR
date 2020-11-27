8ea940f20454cceab36939a035e690b188a17d94
CardRapier <cardrapier@hotmail.com>
2020-11-27 18:12:32 -0500

    test

--- api/views.py
+++ api/views.py
@@ -14,7 +14,7 @@ max_value_songs = 0
 with connection.cursor() as cursor:
     cursor.execute("SELECT MAX(rating) FROM api_song_rating")
     max_value_songs = cursor.fetchone()
-songs = Song.objects.raw('SELECT api_song.* FROM api_song limit 4000')
+songs = Song.objects.raw('SELECT api_song.* FROM api_song')
 songs_most_liked = Song.objects.raw(
     'SELECT api_song.*, SUM(api_song_rating.rating)/count(api_song_rating.rating) AS average FROM api_song, api_song_rating WHERE api_song.song_id=api_song_rating.song_id_id GROUP BY api_song.song_id ORDER BY average DESC')
 song_by_popularity = Song.objects.raw(
@@ -124,7 +124,7 @@ def songDetail(request, pk):
 def songSearch(request):
     search = request.data['search']
     songs = Song.objects.raw(
-        'SELECT * FROM api_song WHERE song_id < 4000 song_title REGEXP %s or song_artist REGEXP %s LIMIT 20', [search, search])
+        'SELECT * FROM api_song WHERE song_title REGEXP %s or song_artist REGEXP %s LIMIT 20', [search, search])
     serializer = SongSerializer(songs, many=True)
     return Response(serializer.data, status.HTTP_200_OK)
 
